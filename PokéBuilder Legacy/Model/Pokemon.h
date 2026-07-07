c61c8e96562c1d5c270f98656d4a9a1e66f15897
Quang Minh Nguyen <quangng711@outlook.com.au>
2026-07-07 15:23:12 +1000

    header file for Pokémon model

--- PokéBuilder Legacy/Model/Pokemon.h
+++ PokéBuilder Legacy/Model/Pokemon.h
@@ -25,5 +25,51 @@ typedef struct {
 // Read-only to prevent accidental overwrites that would corrupt SQLite linkage.
 @property (nonatomic, assign, readonly) NSInteger pokemonID;
 
+// PokéAPI lookup ID. Also read-only since a Pikachu cannot suddenly become a Charizard.
+@property (nonatomic, assign, readonly) NSInteger pokemonNumber;
+
+// copy is used so if a view controller passes an NSMutableString, this object keeps an immutable snapshot, preventing background changes.
+@property (nonatomic, copy) NSString *item;
+@property (nonatomic, copy) NSString *ability;
+@property (nonatomic, copy) NSString *nature;
+
+@property (nonatomic, assign) NSInteger level;
+
+// A C-struct is treated as a primitive block of memory, not an object. Hence no pointing *
+@property (nonatomic, assign) PokemonStats effortValues;
+
+// Ordered collection of NSString Pokémon moves (max 4).
+@property (nonatomic, strong) NSMutableArray *moves;
+
+// Constants
++ (NSInteger)maxLevel;
++ (NSInteger)minLevel;
++ (NSInteger)maxEVs;
++ (NSInteger)minEVs;
++ (NSInteger)maxMoves;
+
+// Init
+- (instancetype)initWithID:(NSInteger)pokemonID
+             pokemonNumber:(NSInteger)pokemonNumber
+                      item:(NSString *)item
+                     level:(NSInteger)level
+                   ability:(NSString *)ability
+              effortValues:(PokemonStats)effortValues
+                    nature:(NSString *)nature
+                     moves:(NSArray *)moves;
+
+// Instance methods
+- (NSString *)getMoveAtIndex:(NSInteger)index;
+
+// Domain logic/validation
++ (NSInteger)clipEV:(NSInteger)value;
++ (NSInteger)validLevel:(NSInteger)level;
++ (NSArray *)validMoves:(NSArray *)moves;
++ (PokemonStats)validEVs:(PokemonStats)stats;
+
+// Generating ID
++ (NSInteger)getUniqueId;
++ (void)resetIdCounterToMaximum:(NSInteger)maximum;
+
 
 @end
