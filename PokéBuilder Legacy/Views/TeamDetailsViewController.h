527cdd02306f57b5f1a62f9277e279785a5a3fdf
Quang Minh Nguyen <quangng711@outlook.com.au>
2026-07-10 00:53:42 +1000

    adding TeamDetailView & minor fixes

---
+++ PokéBuilder Legacy/Views/TeamDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TeamDetailsViewController.h
+//  PokéBuilder Legacy
+//
+//  Created by Quang Minh Nguyen on 9/07/2026.
+//  Copyright (c) 2026 Quang Minh Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TeamDetailsViewController : UIViewController
+
+@end
---
+++ PokéBuilder Legacy/Views/TeamDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  TeamDetailsViewController.m
+//  PokéBuilder Legacy
+//
+//  Created by Quang Minh Nguyen on 9/07/2026.
+//  Copyright (c) 2026 Quang Minh Nguyen. All rights reserved.
+//
+
+#import "TeamDetailsViewController.h"
+
+@interface TeamDetailsViewController ()
+
+@end
+
+@implementation TeamDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
